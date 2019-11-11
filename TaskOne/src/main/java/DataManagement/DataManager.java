ceec6bf9d7b3c5a7641f38345a211dce026bd4b3
BarsantiNicola <barsantinicola9@hotmail.it>
2019-11-12 00:02:40 +0100

    Rimpostato hibernate

--- TaskOne/src/main/java/DataManagement/DataManager.java
+++ TaskOne/src/main/java/DataManagement/DataManager.java
@@ -16,42 +16,42 @@ public class DataManager{
     private final static HConnector HIBERNATE = new HConnector();
     
 
-    public static List<User> searchUsers(String SEARCHED_STRING ){ return MYSQL.searchUsers( SEARCHED_STRING); }
+    public static List<User> searchUsers(String SEARCHED_STRING ){ return HIBERNATE.searchUsers( SEARCHED_STRING); }
     
-    public static List<Employee> searchTeamEmployees( int TEAM_ID , String SEARCHED_VALUE ){ return MYSQL.searchTeamEmployees( TEAM_ID , SEARCHED_VALUE ); }
+    public static List<Employee> searchTeamEmployees( int TEAM_ID , String SEARCHED_VALUE ){ return HIBERNATE.searchTeamEmployees( TEAM_ID , SEARCHED_VALUE ); }
 
-    public static List<Product> searchTeamProducts( int TEAM_ID , String SEARCHED_VALUE ){ return MYSQL.searchTeamProducts( TEAM_ID , SEARCHED_VALUE ); }
+    public static List<Product> searchTeamProducts( int TEAM_ID , String SEARCHED_VALUE ){ return HIBERNATE.searchTeamProducts( TEAM_ID , SEARCHED_VALUE ); }
 
-    public static List<Product> searchProducts( String SEARCHED_STRING ){ return MYSQL.searchProducts( SEARCHED_STRING ); }
+    public static List<Product> searchProducts( String SEARCHED_STRING ){ return HIBERNATE.searchProducts( SEARCHED_STRING ); }
     
-    public static List<Order> searchOrders( String SEARCHED_VALUE , String CUSTOMER_ID ){ return MYSQL.searchOrders( SEARCHED_VALUE , CUSTOMER_ID );};
+    public static List<Order> searchOrders( String SEARCHED_VALUE , String CUSTOMER_ID ){ return HIBERNATE.searchOrders( SEARCHED_VALUE , CUSTOMER_ID );};
     
-    public static List<User> getUsers(){ return MYSQL.getUsers();  }
+    public static List<User> getUsers(){ return HIBERNATE.getUsers();  }
 
-    public static List<Order> getOrder( String CUSTOMER_ID ){ return MYSQL.getOrder( CUSTOMER_ID );  };
+    public static List<Order> getOrder( String CUSTOMER_ID ){ return HIBERNATE.getOrder( CUSTOMER_ID );  };
     
-    public static List<Product> getTeamProducts( int TEAM_ID ){ return MYSQL.getTeamProducts( TEAM_ID ); }
+    public static List<Product> getTeamProducts( int TEAM_ID ){ return HIBERNATE.getTeamProducts( TEAM_ID ); }
 
-    public static List<Employee> getTeamEmployees(int TEAM_ID ){ return MYSQL.getTeamEmployees( TEAM_ID ); }
+    public static List<Employee> getTeamEmployees(int TEAM_ID ){ return HIBERNATE.getTeamEmployees( TEAM_ID ); }
     
-    public static List<Product> getAvailableProducts(){ return MYSQL.getAvailableProducts(); }
+    public static List<Product> getAvailableProducts(){ return HIBERNATE.getAvailableProducts(); }
 
-    public static int getProductType( String PRODUCT_NAME ){ return MYSQL.getProductType( PRODUCT_NAME ); }
+    public static int getProductType( String PRODUCT_NAME ){ return HIBERNATE.getProductType( PRODUCT_NAME ); }
     
-    public static int getTeam( String MANAGER ){ return MYSQL.getTeam( MANAGER ); }
+    public static int getTeam( String MANAGER ){ return HIBERNATE.getTeam( MANAGER ); }
 
-    public static boolean insertUser( User NEW_USER ){ return MYSQL.insertUser( NEW_USER ); }
+    public static boolean insertUser( User NEW_USER ){ return HIBERNATE.insertUser( NEW_USER ); }
     
-    public static boolean insertOrder( String CUSTOMER_ID , int PRODUCT_ID , int PRICE ){ return MYSQL.insertOrder( CUSTOMER_ID , PRODUCT_ID , PRICE ); }
+    public static boolean insertOrder( String CUSTOMER_ID , int PRODUCT_ID , int PRICE ){ return HIBERNATE.insertOrder( CUSTOMER_ID , PRODUCT_ID , PRICE ); }
 
-    public static boolean updateSalary(int SALARY , String USER_ID  ){ return MYSQL.updateSalary( SALARY , USER_ID ); }
+    public static boolean updateSalary(int SALARY , String USER_ID  ){ return HIBERNATE.updateSalary( SALARY , USER_ID ); }
     
-    public static boolean updateProductAvailability( String PRODUCT_NAME , int ADDED_AVAILABILITY ){ return MYSQL.updateProductAvailability( PRODUCT_NAME , ADDED_AVAILABILITY ); }
+    public static boolean updateProductAvailability( String PRODUCT_NAME , int ADDED_AVAILABILITY ){ return HIBERNATE.updateProductAvailability( PRODUCT_NAME , ADDED_AVAILABILITY ); }
 
-    public static boolean deleteUser( String USER_NAME ){ return MYSQL.deleteUser( USER_NAME ); }
+    public static boolean deleteUser( String USER_NAME ){ return HIBERNATE.deleteUser( USER_NAME ); }
 
-    public static UserType login( String USERNAME , String PASSWORD ){ return MYSQL.login( USERNAME , PASSWORD ); }
+    public static UserType login( String USERNAME , String PASSWORD ){ return HIBERNATE.login( USERNAME , PASSWORD ); }
 
-    public static int getMinIDProduct( String PRODUCT_NAME ){ return MYSQL.getMinIDProduct( MYSQL.getProductType(PRODUCT_NAME )); }
+    public static int getMinIDProduct( String PRODUCT_NAME ){ return HIBERNATE.getMinIDProduct(PRODUCT_NAME ); }
 
 }
